bf49ae4b2d78dfa099270825b576f8f86bee675f
Igor Manoryk <imanoryk6@gmail.com>
2023-01-25 20:06:32 +0100

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const { findOne } = require('./db-functions');
 const { generateAccessToken, verifyAccessToken } = require('./auth-functions');
 
 const port = 3000;
 const app = express();
 
+app.use(cors({ origin: ['localhost:4200', '127.0.0.1:4200'] }));
 app.use(express.json());
 
 app.post('/login', async (req, res) => {
