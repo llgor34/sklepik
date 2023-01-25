297e33382b81bec6d96e65d1d2fbd972d2daac23
Igor Manoryk <imanoryk6@gmail.com>
2023-01-25 20:03:17 +0100

    login panel stable

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  form!: FormGroup;
 
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      password: ['', Validators.required],
+    });
+  }
+
+  get passwordControl() {
+    return this.form.controls['password'];
+  }
+
+  formSubmit() {}
 }
