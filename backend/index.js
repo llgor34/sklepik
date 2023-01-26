ce484cb1f6c2ee73decba56de79fdf233165080d
Igor Manoryk <imanoryk6@gmail.com>
2023-01-26 17:47:29 +0100

    updated interfaces on backend

--- backend/index.js
+++ backend/index.js
@@ -11,23 +11,19 @@ app.use(express.json());
 
 app.post('/login', async (req, res) => {
 	const password = req.body?.password;
-
 	if (!password) {
-		return res
-			.status(422)
-			.send({ ok: false, message: 'NO_PASSWORD_PROVIDED' });
+		return res.status(422).send({ ok: false, message: 'NO_PASSWORD_PROVIDED' });
 	}
 
 	const user = await findOne('pracownicy', { haslo: password });
-
 	if (!user) {
 		return res.status(401).send({ ok: false, message: 'USER_NOT_FOUND' });
 	}
 
 	const token = generateAccessToken(user);
-
 	return res.send({
 		ok: true,
+		message: 'USER_LOGGED_IN',
 		token,
 		user: { name: user.imie, surname: user.nazwisko, role: user.role },
 	});
