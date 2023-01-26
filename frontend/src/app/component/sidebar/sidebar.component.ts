7f630e1229563321c22f16c98797fee250a2b253
Igor Manoryk <imanoryk6@gmail.com>
2023-01-26 14:08:34 +0100

    added sidebar

--- frontend/src/app/component/sidebar/sidebar.component.ts
+++ frontend/src/app/component/sidebar/sidebar.component.ts
@@ -3,5 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
+  styleUrls: ['sidebar.component.css'],
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  isOpen = true;
+
+  toggleIsOpen() {
+    this.isOpen = !this.isOpen;
+  }
+}
