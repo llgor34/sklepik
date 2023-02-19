a0631080851015cb6a83e1deebd5f09fe90eb348
Igor Manoryk <imanoryk6@gmail.com>
2023-02-19 17:23:50 +0100

    created frontend page for raports

---
+++ frontend/src/app/interfaces/raport.interface.ts
@@ -0,0 +1,6 @@
+export interface Raport {
+  imageUrl: string;
+  title: string;
+  description: string;
+  url: string;
+}
--- frontend/src/app/pages/raport/raport.component.ts
+++ frontend/src/app/pages/raport/raport.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { Raport } from 'src/app/interfaces/raport.interface';
 
 @Component({
   selector: 'app-raport',
   templateUrl: './raport.component.html',
+  styleUrls: ['./raport.component.css'],
 })
-export class RaportComponent {}
+export class RaportComponent {
+  raports: Raport[] = [
+    {
+      imageUrl: 'assets/img1.png',
+      title: 'Protokół zamknięcia',
+      description: 'Wygeneruj dzienny raport zamknięcia sklepiku.',
+      url: '/closement-protocol',
+    },
+  ];
+}
