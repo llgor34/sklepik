cdcd0ca044c03237dbf63da80bbaf2754ebc10c1
Igor Manoryk <imanoryk6@gmail.com>
2024-01-16 14:21:21 +0100

    added possibility to add negative number of product for admin

--- backend/general/auth-functions.mjs
+++ backend/general/auth-functions.mjs
@@ -7,45 +7,49 @@ import { sendErrorMessage } from './messages.mjs';
 dotenv.config();
 
 export function generateAccessToken(user) {
-	return jwt.sign({ id: user.id, roles: user.roles }, process.env.TOKEN_SECRET, {
-		expiresIn: `${process.env.TOKEN_EXPIRATION_TIME_IN_SECONDS}s`,
-	});
+    return jwt.sign({ id: user.id, roles: user.roles }, process.env.TOKEN_SECRET, {
+        expiresIn: `${process.env.TOKEN_EXPIRATION_TIME_IN_SECONDS}s`,
+    });
 }
 
 export function verifyAccessToken(req, res, next) {
-	const token = req.signedCookies.jwt;
+    const token = req.signedCookies.jwt;
 
-	if (!token) {
-		return sendErrorMessage(res, 401, 'AUTH_TOKEN_NOT_PROVIDED');
-	}
+    if (!token) {
+        return sendErrorMessage(res, 401, 'AUTH_TOKEN_NOT_PROVIDED');
+    }
 
-	jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
-		if (err) {
-			res.clearCookie('jwt');
-			return sendErrorMessage(res, 401, 'AUTH_TOKEN_EXPIRED');
-		}
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            res.clearCookie('jwt');
+            return sendErrorMessage(res, 401, 'AUTH_TOKEN_EXPIRED');
+        }
 
-		req.user = decoded;
-		next();
-	});
+        req.user = decoded;
+        next();
+    });
 }
 
 export function verifyPassword(password, hash) {
-	return hashPassword(password) === hash;
+    return hashPassword(password) === hash;
 }
 
 export function hashPassword(password) {
-	const hash = createHash('sha256').update(password).digest('hex');
-	return hash;
+    const hash = createHash('sha256').update(password).digest('hex');
+    return hash;
 }
 
 export function signJWTCookie(res, token) {
-	res.cookie('jwt', token, { signed: true, httpOnly: true, expires: new Date(Date.now() + process.env.TOKEN_EXPIRATION_TIME_IN_SECONDS * 1000) });
+    res.cookie('jwt', token, { signed: true, httpOnly: true, expires: new Date(Date.now() + process.env.TOKEN_EXPIRATION_TIME_IN_SECONDS * 1000) });
 }
 
-export function hasRole(req, res, next, role) {
-	if (!req.user.roles?.includes(role)) {
-		return sendErrorMessage(res, 401, 'INSUFFICIENT_PERMISSIONS');
-	}
-	next();
+export function hasRoleMiddleware(req, res, next, role) {
+    if (!hasRole(req.user.roles, role)) {
+        return sendErrorMessage(res, 401, 'INSUFFICIENT_PERMISSIONS');
+    }
+    next();
+}
+
+export function hasRole(userRoles, requiredRole) {
+    return userRoles?.includes(requiredRole);
 }
--- backend/index.mjs
+++ backend/index.mjs
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 import moment from 'moment';
 import fs from 'fs';
 
-import { generateAccessToken, verifyAccessToken, signJWTCookie, hasRole, hashPassword } from './general/auth-functions.mjs';
+import { generateAccessToken, verifyAccessToken, signJWTCookie, hasRoleMiddleware, hasRole, hashPassword } from './general/auth-functions.mjs';
 import { getUserByPassword } from './db/auth.mjs';
 import { getArticleById } from './db/articles.mjs';
 import { sendErrorMessage } from './general/messages.mjs';
@@ -29,187 +29,189 @@ app.use(express.json());
 app.use(cookieParser(process.env.TOKEN_SECRET));
 
 app.get('/generate-password/:password', async (req, res) => {
-	const { password } = req.params;
-	const hash = hashPassword(password);
-	return res.send(hash);
+    const { password } = req.params;
+    const hash = hashPassword(password);
+    return res.send(hash);
 });
 
 app.post('/login', async (req, res) => {
-	const { password } = req.body;
-	if (!password) {
-		return sendErrorMessage(res, 422, 'PASSWORD_NOT_PROVIDED');
-	}
+    const { password } = req.body;
+    if (!password) {
+        return sendErrorMessage(res, 422, 'PASSWORD_NOT_PROVIDED');
+    }
 
-	const user = await getUserByPassword(password);
-	if (!user) {
-		return sendErrorMessage(res, 401, 'USER_NOT_FOUND');
-	}
+    const user = await getUserByPassword(password);
+    if (!user) {
+        return sendErrorMessage(res, 401, 'USER_NOT_FOUND');
+    }
 
-	const token = generateAccessToken(user);
-	signJWTCookie(res, token);
+    const token = generateAccessToken(user);
+    signJWTCookie(res, token);
 
-	const { name, surname, roles } = user;
-	return res.send({
-		ok: true,
-		message: 'USER_LOGGED_IN',
-		user: { name, surname, roles },
-	});
+    const { name, surname, roles } = user;
+    return res.send({
+        ok: true,
+        message: 'USER_LOGGED_IN',
+        user: { name, surname, roles },
+    });
 });
 
 // app.get('/', verifyAccessToken, (req, res) => res.send(req.user));
 
 app.get('/product/:id', verifyAccessToken, async (req, res) => {
-	const productCode = req.params.id;
-	const product = await getArticleById(productCode);
+    const productCode = req.params.id;
+    const product = await getArticleById(productCode);
 
-	return res.send({ ok: true, message: 'SUCCESS', product: product });
+    return res.send({ ok: true, message: 'SUCCESS', product: product });
 });
 
 app.get('/coffee-subscribers', verifyAccessToken, async (req, res) => {
-	const coffeeSubscribers = await getAllCoffeeSubscriptions();
-	res.send({ ok: true, message: 'SUCCESS', coffeeSubscribers });
+    const coffeeSubscribers = await getAllCoffeeSubscriptions();
+    res.send({ ok: true, message: 'SUCCESS', coffeeSubscribers });
 });
 
 app.get('/coffee-subscribers/update/:clientId/:amount', verifyAccessToken, async (req, res) => {
-	const clientId = +req.params.clientId;
-	const amount = +req.params.amount;
+    const clientId = +req.params.clientId;
+    const amount = +req.params.amount;
 
-	await updateCoffeeSubscriptionByAmount(clientId, req.user.id, amount);
-	res.send({ ok: true, message: 'SUCCESS' });
+    await updateCoffeeSubscriptionByAmount(clientId, req.user.id, amount);
+    res.send({ ok: true, message: 'SUCCESS' });
 });
 
 app.get('/coffee-subscribers/receive-coffee/:clientId', verifyAccessToken, async (req, res) => {
-	const clientId = +req.params.clientId;
+    const clientId = +req.params.clientId;
 
-	await updateCoffeeSubscriptionByReceiveCoffee(clientId, req.user.id);
-	res.send({ ok: true, message: 'SUCCESS' });
+    await updateCoffeeSubscriptionByReceiveCoffee(clientId, req.user.id);
+    res.send({ ok: true, message: 'SUCCESS' });
 });
 
 app.post('/order/create', verifyAccessToken, async (req, res) => {
-	const { products, paymentMethod } = req.body;
+    const { products, paymentMethod } = req.body;
 
-	let totalPrice = 0;
-	for (const product of products) {
-		totalPrice += +product.price * product.amount;
-	}
-	if (totalPrice < 0) {
-		return sendErrorMessage(res, 422, 'NEGATIVE_PRICE');
-	}
+    let totalPrice = 0;
+    for (const product of products) {
+        totalPrice += +product.price * product.amount;
+    }
+    if (totalPrice < 0) {
+        if (!hasRole(req.user.roles, 'admin')) {
+            return sendErrorMessage(res, 422, 'NEGATIVE_PRICE');
+        }
+    }
 
-	const discounts = products.filter(product => product.type === 'discount');
-	for (const discount of discounts) {
-		const usedDiscount = await getUsedDiscount(discount.code);
-		const owedDiscount = await getOwedDiscount(discount.code);
+    const discounts = products.filter(product => product.type === 'discount');
+    for (const discount of discounts) {
+        const usedDiscount = await getUsedDiscount(discount.code);
+        const owedDiscount = await getOwedDiscount(discount.code);
 
-		const leftDiscount = owedDiscount + usedDiscount + discount.amount * +discount.price;
-		if (leftDiscount < 0) {
-			return sendErrorMessage(res, 422, 'DISCOUNT_TOO_HIGH');
-		}
-	}
+        const leftDiscount = owedDiscount + usedDiscount + discount.amount * +discount.price;
+        if (leftDiscount < 0) {
+            return sendErrorMessage(res, 422, 'DISCOUNT_TOO_HIGH');
+        }
+    }
 
-	if (products.length < 1) {
-		return sendErrorMessage(res, 422, 'PRODUCTS_NOT_PROVIDED');
-	}
+    if (products.length < 1) {
+        return sendErrorMessage(res, 422, 'PRODUCTS_NOT_PROVIDED');
+    }
 
-	await createOrder(products, paymentMethod, req.user.id);
-	res.send({ ok: true, message: 'SELL_CREATED' });
+    await createOrder(products, paymentMethod, req.user.id);
+    res.send({ ok: true, message: 'SELL_CREATED' });
 });
 
 app.get('/raports/sellment-close/latest-raport-preview', verifyAccessToken, async (req, res) => {
-	const data = await getRaport();
-	res.send({ ok: true, message: 'SUCCESS', data });
+    const data = await getRaport();
+    res.send({ ok: true, message: 'SUCCESS', data });
 });
 
 app.get(
-	'/raports/sellment-close/generate-raport',
-	verifyAccessToken,
-	(...args) => hasRole(...args, 'admin'),
-	async (req, res) => {
-		const raportInfo = await generateRaport(req.user.id);
+    '/raports/sellment-close/generate-raport',
+    verifyAccessToken,
+    (...args) => hasRoleMiddleware(...args, 'admin'),
+    async (req, res) => {
+        const raportInfo = await generateRaport(req.user.id);
 
-		if (!raportInfo) {
-			return sendErrorMessage(res, 409, 'RAPORT_NOT_GENERATED');
-		}
+        if (!raportInfo) {
+            return sendErrorMessage(res, 409, 'RAPORT_NOT_GENERATED');
+        }
 
-		const raport = await getRaport(raportInfo.id);
-		const date = moment(raportInfo.date).format('DD.MM.YY');
+        const raport = await getRaport(raportInfo.id);
+        const date = moment(raportInfo.date).format('DD.MM.YY');
 
-		const raportPath = await generateRaportPDF(raport, date, raportInfo.number, raportInfo.year_number);
-		const pdfFile = fs.readFileSync(raportPath);
-		fs.rmSync(raportPath);
+        const raportPath = await generateRaportPDF(raport, date, raportInfo.number, raportInfo.year_number);
+        const pdfFile = fs.readFileSync(raportPath);
+        fs.rmSync(raportPath);
 
-		res.contentType('application/pdf');
-		res.send(pdfFile);
-	}
+        res.contentType('application/pdf');
+        res.send(pdfFile);
+    }
 );
 
 app.get(
-	'/workers/get',
-	verifyAccessToken,
-	(...args) => hasRole(...args, 'admin'),
-	async (req, res) => {
-		const workers = await getWorkers();
-		res.send({ ok: true, message: 'SUCCESS', workers });
-	}
+    '/workers/get',
+    verifyAccessToken,
+    (...args) => hasRoleMiddleware(...args, 'admin'),
+    async (req, res) => {
+        const workers = await getWorkers();
+        res.send({ ok: true, message: 'SUCCESS', workers });
+    }
 );
 
 app.get('/workers/get-used-discount/:id', verifyAccessToken, async (req, res) => {
-	const workerCode = +req.params.id;
-	const usedDiscount = await getUsedDiscount(workerCode);
+    const workerCode = +req.params.id;
+    const usedDiscount = await getUsedDiscount(workerCode);
 
-	res.send({ ok: true, message: 'SUCCESS', usedDiscount });
+    res.send({ ok: true, message: 'SUCCESS', usedDiscount });
 });
 
 app.get('/workers/get-owed-discount/:id', verifyAccessToken, async (req, res) => {
-	const workerCode = +req.params.id;
-	const owedDiscount = await getOwedDiscount(workerCode);
+    const workerCode = +req.params.id;
+    const owedDiscount = await getOwedDiscount(workerCode);
 
-	res.send({ ok: true, message: 'SUCCESS', owedDiscount });
+    res.send({ ok: true, message: 'SUCCESS', owedDiscount });
 });
 
 app.get(
-	'/hours-settlement/get',
-	verifyAccessToken,
-	(...args) => hasRole(...args, 'admin'),
-	async (req, res) => {
-		const hoursSettlement = await getHoursSettlement();
-		res.send({ ok: true, message: 'SUCCESS', hoursSettlement });
-	}
+    '/hours-settlement/get',
+    verifyAccessToken,
+    (...args) => hasRoleMiddleware(...args, 'admin'),
+    async (req, res) => {
+        const hoursSettlement = await getHoursSettlement();
+        res.send({ ok: true, message: 'SUCCESS', hoursSettlement });
+    }
 );
 
 app.get(
-	'/hours-settlement/delete/:id',
-	verifyAccessToken,
-	(...args) => hasRole(...args, 'admin'),
-	async (req, res) => {
-		const hoursSettlementId = +req.params.id;
-		await deleteHoursSettlement(hoursSettlementId);
+    '/hours-settlement/delete/:id',
+    verifyAccessToken,
+    (...args) => hasRoleMiddleware(...args, 'admin'),
+    async (req, res) => {
+        const hoursSettlementId = +req.params.id;
+        await deleteHoursSettlement(hoursSettlementId);
 
-		res.send({ ok: true, message: 'SUCCESS' });
-	}
+        res.send({ ok: true, message: 'SUCCESS' });
+    }
 );
 
 app.get(
-	'/activities/get',
-	verifyAccessToken,
-	(...args) => hasRole(...args, 'admin'),
-	async (req, res) => {
-		const activities = await getActivities();
-		res.send({ ok: true, message: 'SUCCESS', activities });
-	}
+    '/activities/get',
+    verifyAccessToken,
+    (...args) => hasRoleMiddleware(...args, 'admin'),
+    async (req, res) => {
+        const activities = await getActivities();
+        res.send({ ok: true, message: 'SUCCESS', activities });
+    }
 );
 
 app.post(
-	'/activities/create',
-	verifyAccessToken,
-	(...args) => hasRole(...args, 'admin'),
-	async (req, res) => {
-		const { workerId, activityId, description, date, amount } = req.body;
-		await createActivity(activityId, req.user.id, workerId, amount, date, description);
-		res.send({ ok: true, message: 'SUCCESS' });
-	}
+    '/activities/create',
+    verifyAccessToken,
+    (...args) => hasRoleMiddleware(...args, 'admin'),
+    async (req, res) => {
+        const { workerId, activityId, description, date, amount } = req.body;
+        await createActivity(activityId, req.user.id, workerId, amount, date, description);
+        res.send({ ok: true, message: 'SUCCESS' });
+    }
 );
 
 app.listen(port, () => {
-	console.log(`⚡ Server running at: http://localhost:${port}`);
+    console.log(`⚡ Server running at: http://localhost:${port}`);
 });
