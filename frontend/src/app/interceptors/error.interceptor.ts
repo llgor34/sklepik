21734155cabe62f7c43ea404cbc9915906ed8a1b
Igor Manoryk <imanoryk6@gmail.com>
2024-03-05 12:13:55 +0100

    error handling refactor

--- frontend/src/app/interceptors/error.interceptor.ts
+++ frontend/src/app/interceptors/error.interceptor.ts
@@ -18,10 +18,29 @@ export class ErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler) {
     return next.handle(request).pipe(
       catchError(({ error }) => {
+        console.log(error);
         switch (error.message) {
           case 'AUTH_TOKEN_NOT_PROVIDED':
           case 'AUTH_TOKEN_EXPIRED':
             this.authService.logout();
+            break;
+
+          case 'PASSWORD_NOT_PROVIDED':
+            this.toastService.showError('Nie podano hasła');
+            break;
+
+          case 'PRODUCTS_NOT_PROVIDED':
+            this.toastService.showError('Nie przekazano żadnych produktów');
+            break;
+
+          case 'INSUFFICIENT_PERMISSIONS':
+            this.toastService.showError(
+              'Nie posiadasz wystarczających uprawnień'
+            );
+            break;
+
+          case 'RAPORT_NOT_GENERATED':
+            this.toastService.showError('Nie udało się wygenerować raportu');
             break;
 
           case 'DISCOUNT_TOO_HIGH':
@@ -29,8 +48,15 @@ export class ErrorInterceptor implements HttpInterceptor {
               'Kwota zniżki nie może przekraczać łącznej kwoty wypracowanych zniżek'
             );
             break;
+
           case 'NEGATIVE_PRICE':
             this.toastService.showError('Kwota zamówienia nie może być ujemna');
+            break;
+
+          case 'USER_NOT_FOUND':
+            this.toastService.showError(
+              'Użytkownik o podanym identyfikatorze nie istnieje!'
+            );
             break;
 
           default:
--- frontend/src/app/interfaces/errorResponse.interface.ts
+++ frontend/src/app/interfaces/errorResponse.interface.ts
@@ -2,13 +2,11 @@ import { Response } from './response.interface';
 
 export interface ErrorResponse extends Response {
   message:
-    | 'PASSWORD_NOT_PROVIDED'
-    | 'USERNAME_NOT_PROVIDED'
-    | 'USER_NOT_FOUND'
-    | 'PASSWORD_INVALID'
-    | 'PRODUCTS_NOT_PROVIDED'
     | 'AUTH_TOKEN_NOT_PROVIDED'
     | 'AUTH_TOKEN_EXPIRED'
+    | 'PASSWORD_NOT_PROVIDED'
+    | 'PRODUCTS_NOT_PROVIDED'
+    | 'USER_NOT_FOUND'
     | 'INSUFFICIENT_PERMISSIONS'
     | 'RAPORT_NOT_GENERATED'
     | 'DISCOUNT_TOO_HIGH'
