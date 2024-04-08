7ae49aee2d4e9e274d4d589278dceff46081882c
Igor Manoryk <imanoryk6@gmail.com>
2024-04-08 20:45:10 +0200

    local variable changes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@ import { Environment } from 'src/app/interfaces/environment.interface';
 
 export const environment: Environment = {
     itemsPerPage: 20,
-    wsAddress: 'ws://192.168.0.1:3001',
+    wsAddress: 'http://localhost:3001',
     wsConfig: {
         withCredentials: true,
     },
