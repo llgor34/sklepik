775ee253ca0dbdad88eed8077cbb860d34d25841
Igor Manoryk <imanoryk6@gmail.com>
2024-05-01 13:27:57 +0200

    stable, before refactor

--- frontend/src/app/pages/panel-users/panel-users.component.ts
+++ frontend/src/app/pages/panel-users/panel-users.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TableBodyContext } from 'src/app/interfaces/table-body-context.interface';
+import { User } from 'src/app/interfaces/user.interface';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
-  selector: 'app-panel-users',
-  templateUrl: './panel-users.component.html',
-  styleUrls: ['./panel-users.component.css']
+    selector: 'app-panel-users',
+    templateUrl: './panel-users.component.html',
+    styleUrls: ['./panel-users.component.css'],
 })
 export class PanelUsersComponent {
+    users$: Observable<User[]> = this.userService.getUsers$();
+    userType!: TableBodyContext<User[]>;
 
+    constructor(private userService: UserService) {}
 }
