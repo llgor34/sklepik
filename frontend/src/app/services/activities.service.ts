550764ff749767560ebef625d733c9a0ad9366c0
Igor Manoryk <imanoryk6@gmail.com>
2024-05-09 21:08:59 +0200

    typing improvements for activities.service.ts

--- frontend/src/app/services/activities.service.ts
+++ frontend/src/app/services/activities.service.ts
@@ -1,18 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ActivityCreateForm, ActivityResponse } from '../interfaces/activity.interface';
-import { map } from 'rxjs';
+import { Activity, ActivityCreateForm, ActivityResponse } from '../interfaces/activity.interface';
+import { Observable, map } from 'rxjs';
 import { Response } from '../interfaces/response.interface';
 
 @Injectable({ providedIn: 'root' })
 export class ActivitiesService {
     constructor(private http: HttpClient) {}
 
-    getActivities() {
+    getActivities(): Observable<Activity[]> {
         return this.http.get<ActivityResponse>('api/activities/get').pipe(map((res) => res.activities));
     }
 
-    createActivity(form: ActivityCreateForm) {
+    createActivity(form: ActivityCreateForm): Observable<Response> {
         return this.http.post<Response>('api/activities/create', form);
     }
 }
