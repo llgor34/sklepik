76d69b9f900e619ac0e2b6110269938f4d1fb5b8
Igor Manoryk <imanoryk6@gmail.com>
2024-05-23 08:59:50 +0200

    refactored a little bit

--- frontend/src/app/pages/panel-hours-settlement/panel-hours-settlement.component.ts
+++ frontend/src/app/pages/panel-hours-settlement/panel-hours-settlement.component.ts
@@ -8,8 +8,6 @@ import { UserService } from 'src/app/services/user.service';
 import { Observable, map, shareReplay, take, tap } from 'rxjs';
 import { EditableItem } from 'src/app/interfaces/editable-item.interface';
 import { ActivitiesService } from 'src/app/services/activities.service';
-import { User } from 'src/app/interfaces/user.interface';
-import { Activity } from 'src/app/interfaces/activity.interface';
 
 @Component({
     selector: 'app-panel-hours-settlement',
@@ -26,30 +24,8 @@ export class PanelHoursSettlementComponent extends PanelComponent<HoursSettlemen
     @ViewChild(NewHoursSettlementComponent, { static: false })
     newHoursSettlementComponent!: NewHoursSettlementComponent;
 
-    users$: Observable<EditableItem[]> = this.userService.getUsers$().pipe(
-        shareReplay(1),
-        map((users) => this.mapUsersToEditable(users))
-    );
-    activities$: Observable<EditableItem[]> = this.activitiesService.getActivities().pipe(
-        shareReplay(1),
-        map((activities) => this.mapActivitiesToEditable(activities))
-    );
-
-    mapUsersToEditable(users: User[]): EditableItem[] {
-        return users.map<EditableItem>((user) => ({ id: user.id!, label: `${user.name} ${user.surname}` }));
-    }
-
-    mapActivitiesToEditable(activities: Activity[]): EditableItem[] {
-        return activities.map<EditableItem>((activity) => ({ id: activity.id, label: activity.name }));
-    }
-
-    getWorkerEditableById$(id: number): Observable<EditableItem> {
-        return this.users$.pipe(map((users) => users.filter((user) => user.id === id)[0]));
-    }
-
-    getActivityById$(id: number) {
-        return this.activities$.pipe(map((activities) => activities.filter((activity) => activity.id === id)[0]));
-    }
+    users$: Observable<EditableItem[]> = this.userService.getUsersEditable$().pipe(shareReplay(1));
+    activities$: Observable<EditableItem[]> = this.activitiesService.getActivitiesEditable().pipe(shareReplay(1));
 
     ngOnInit(): void {
         super.initializeComponent(
@@ -58,6 +34,14 @@ export class PanelHoursSettlementComponent extends PanelComponent<HoursSettlemen
             this.hoursSettlementService.deleteHoursSettlement$,
             this.hoursSettlementService.createHoursSettlement$
         );
+    }
+
+    getUserEditableById$(id: number): Observable<EditableItem> {
+        return this.userService.getUserEditableById$(this.users$, id);
+    }
+
+    getActivityEditableById$(id: number): Observable<EditableItem> {
+        return this.activitiesService.getActivityEditableById$(this.activities$, id);
     }
 
     onAddNewEmptyRecord() {
--- frontend/src/app/services/activities.service.ts
+++ frontend/src/app/services/activities.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Activity } from '../interfaces/activity.interface';
 import { Observable, map } from 'rxjs';
 import { Response } from '../interfaces/response.interface';
+import { EditableItem } from '../interfaces/editable-item.interface';
 
 @Injectable({ providedIn: 'root' })
 export class ActivitiesService {
@@ -11,4 +12,16 @@ export class ActivitiesService {
     getActivities(): Observable<Activity[]> {
         return this.http.get<Response<Activity[]>>('api/activities').pipe(map((res) => res.data));
     }
+
+    getActivitiesEditable(): Observable<EditableItem[]> {
+        return this.getActivities().pipe(map(this.mapActivitiesToEditable));
+    }
+
+    getActivityEditableById$(activitiesEditable$: Observable<EditableItem[]>, id: number) {
+        return activitiesEditable$.pipe(map((activities) => activities.filter((activity) => activity.id === id)[0]));
+    }
+
+    private mapActivitiesToEditable = (activities: Activity[]): EditableItem[] => {
+        return activities.map<EditableItem>((activity) => ({ id: activity.id, label: activity.name }));
+    };
 }
