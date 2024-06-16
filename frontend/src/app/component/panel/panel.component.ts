6d2c03b96e7244e8d26d638c858834c03a20066f
Igor Manoryk <imanoryk6@gmail.com>
2024-06-16 21:06:29 +0200

    prototype ready

--- frontend/src/app/component/panel/panel.component.ts
+++ frontend/src/app/component/panel/panel.component.ts
@@ -17,14 +17,6 @@ import { ToastService } from 'src/app/services/toast.service';
     styleUrls: ['./panel.component.css'],
 })
 export class PanelComponent<T extends Id> implements OnInit {
-    private subscription = new Subscription();
-    private refreshItems$ = new BehaviorSubject(null);
-
-    private getFn$!: GetFn<T>;
-    private updateFn$!: UpdateFn;
-    private deleteFn$!: DeleteFn;
-    private createFn$!: CreateFn;
-
     toastService: ToastService = inject(ToastService);
     newRecordService: NewRecordService<T> = inject(NewRecordService);
     panelService: PanelService<T> = inject(PanelService);
@@ -32,25 +24,26 @@ export class PanelComponent<T extends Id> implements OnInit {
     observable$!: Observable<T[]>;
     observableContext!: TableBodyContext<(T & NumeratedIdx)[]>;
 
-    ngOnInit(): void {
-        console.log('RUNNED');
-        this.initializeComponent(
-            this.panelService.getRecords$,
-            this.panelService.updateRecord$,
-            this.panelService.deleteRecord$,
-            this.panelService.createRecord$
-        );
+    private subscription = new Subscription();
+    private refreshItems$ = new BehaviorSubject(null);
+
+    onTableHeaderSort(columnKey: string): void {
+        if (this.panelService.getOrderBy() === columnKey) {
+            this.panelService.toggleOrderType();
+        }
+        this.panelService.setOrderBy(columnKey);
+        this.refreshItems();
     }
 
     onCreateItem(item: T) {
-        this.createFn$(item).subscribe((id) => {
+        this.panelService.createRecord$(item).subscribe((id) => {
             this.refreshItems();
             this.showCreateSuccess(id);
         });
     }
 
     onDeleteItem(id: number) {
-        this.deleteFn$(id).subscribe(() => {
+        this.panelService.deleteRecord$(id).subscribe(() => {
             this.refreshItems();
             this.showDeleteSuccess(id);
         });
@@ -59,7 +52,7 @@ export class PanelComponent<T extends Id> implements OnInit {
     onFieldChange(item: T & NumeratedIdx, obj: Partial<T>) {
         const key: string = Object.keys(obj)[0];
 
-        this.updateFn$(item.id!, { [key]: (obj as any)[key] }).subscribe(() => {
+        this.panelService.updateRecord$(item.id!, { [key]: (obj as any)[key] }).subscribe(() => {
             this.refreshItems();
             this.showUpdateSuccess(item.id!);
         });
@@ -93,45 +86,16 @@ export class PanelComponent<T extends Id> implements OnInit {
         return item.id;
     }
 
-    protected initializeComponent(
-        getFn$: GetFn<T>,
-        updateFn$: UpdateFn,
-        deleteFn$: DeleteFn,
-        createFn$: CreateFn
-    ): void {
-        this.bindHttpFunctions(updateFn$, deleteFn$, getFn$, createFn$);
+    ngOnInit(): void {
         this.createRefreshObservable();
         this.listenForRecordSubmitEvent();
     }
 
     private createRefreshObservable(): void {
         this.observable$ = this.refreshItems$.pipe(
-            switchMap(() => this.getFn$()),
+            switchMap(() => this.panelService.getRecords$()),
             shareReplay(1)
         );
-    }
-
-    private bindHttpFunctions(updateFn$: UpdateFn, deleteFn$: DeleteFn, getFn$: GetFn<T>, createFn$: CreateFn): void {
-        this.bindUpdateFn(updateFn$);
-        this.bindDeleteFn(deleteFn$);
-        this.bindGetFn(getFn$);
-        this.bindCreateFn(createFn$);
-    }
-
-    private bindUpdateFn(updateFn$: UpdateFn) {
-        this.updateFn$ = updateFn$;
-    }
-
-    private bindDeleteFn(deleteFn$: DeleteFn) {
-        this.deleteFn$ = deleteFn$;
-    }
-
-    private bindGetFn(getFn$: GetFn<T>) {
-        this.getFn$ = getFn$;
-    }
-
-    private bindCreateFn(createFn$: CreateFn) {
-        this.createFn$ = createFn$;
     }
 
     private listenForRecordSubmitEvent(): void {
--- frontend/src/app/services/panel.service.ts
+++ frontend/src/app/services/panel.service.ts
@@ -1,19 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { map } from 'rxjs';
 import { GetFn } from '../interfaces/get-fn.interface';
 import { UpdateFn, updateObj } from '../interfaces/update-fn.interface';
 import { DeleteFn } from '../interfaces/delete-fn.interface';
 import { CreateFn } from '../interfaces/create-fn.interface';
 import { Response } from '../interfaces/response.interface';
 import { PANEL_SERVICE_ENDPOINT } from '../constants/endpoint.constant';
+import { OrderType } from '../interfaces/order-type.interface';
 
 @Injectable()
 export class PanelService<T> {
     protected http = inject(HttpClient);
     protected endpoint: string = inject(PANEL_SERVICE_ENDPOINT);
 
+    protected orderBy: string | null = null;
+    protected orderType: OrderType = 'desc';
+
+    setOrderBy(orderBy: string): void {
+        this.orderBy = orderBy;
+    }
+
+    getOrderBy(): string | null {
+        return this.orderBy;
+    }
+
+    toggleOrderType(): void {
+        this.orderType = this.orderType === 'asc' ? 'desc' : 'asc';
+    }
+
     getRecords$: GetFn<T> = () => {
+        if (this.orderBy) {
+            return this.http
+                .get<Response<T[]>>(this.endpoint, { params: { orderBy: this.orderBy, orderType: this.orderType } })
+                .pipe(map((res) => res.data));
+        }
         return this.http.get<Response<T[]>>(this.endpoint).pipe(map((res) => res.data));
     };
 
